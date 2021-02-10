35b50aa597241ec0bcd8e0b9a4910aa28bda0caf
eminemtnn27 <eminemetin9@gmail.com>
2021-02-10 18:53:07 +0300

    bootstrap kurulumu yapıldı

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,3 +2,4 @@
     <x-slot name="header"> Anasayfa   </x-slot>
     <div class="alert alert-danger">Bootstrap alert Mesajı</div>
 </x-app-layout>
+ 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
              
                 {{ $slot }} 
-              
+                 
         </div>
     </div>
         </div>
