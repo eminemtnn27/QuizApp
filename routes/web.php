7adef381948b105d80a635477611f71b70ccc20d
eminemtnn27 <eminemetin9@gmail.com>
2021-02-12 17:40:10 +0300

    bootstrap kurulumu yapıldı middleware eklendi

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+Route::middleware(['auth', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::group(['middleware'=>['auth','isAdmin'],'prefix'=>'admin'],function(){
+
+});
