b858c762e7238aaeb06cb8eda5af0389b093d4ff
eminemtnn27 <eminemetin9@gmail.com>
2021-02-20 22:58:21 +0300

    düzenleme yapıldı

--- app/Http/Requests/QuizCreateRequest.php
+++ app/Http/Requests/QuizCreateRequest.php
@@ -11,7 +11,7 @@ class QuizCreateRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize()             
     {
         return false;
     }
